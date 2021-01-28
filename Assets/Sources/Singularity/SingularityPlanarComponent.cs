7b04c2218c40ea05f93c2aa827e9b348b6a0892c
GuillaumeBiege <guillaume.biege@gmail.com>
2021-01-28 18:08:47 +0100

    Update SingularityPlanarComponent.cs
    Correction on the target to plane projection

--- Assets/Sources/Singularity/SingularityPlanarComponent.cs
+++ Assets/Sources/Singularity/SingularityPlanarComponent.cs
@@ -6,50 +6,102 @@ public class SingularityPlanarComponent : SingularityComponent
 {
     [SerializeField] GameObject m_rSourceB = null;
     [SerializeField] GameObject m_rSourceC = null;
+    [SerializeField] Vector3 m_vPosSourceD = Vector3.zero;
 
-    bool CheckIfWithinSegement(Vector3 _posProjected, Vector3 _axialVector)
+    Vector3 PosProjected = Vector3.zero;
+
+
+    //Check if the projected point is within the quad formed by the four points of the singularity
+    bool CheckIfWithinSegement(Vector3 _posProjected, Vector3 _planeNormal)
     {
-        Vector3 SourceToProj = _posProjected - transform.position;
-        Vector3 TargetToProj = _posProjected - m_rSourceB.transform.position;
+        //     ABl
+        //      |
+        //      A-------B-- BCl
+        //      |       |
+        //      |       |
+        //      |       |
+        // CAl--C-------D
+        //              |
+        //             DCl
 
-        if (Vector3.Dot(SourceToProj, _axialVector) < 0)
+        //To store the result of each detectection
+        bool[] check = new bool[4];
+
+        //Calculate Point D position
+        m_vPosSourceD = (m_rSourceB.transform.position - transform.position) + (m_rSourceC.transform.position - transform.position) + transform.position;
+
+
+        //For AB
+        check[0] = DotProductSegementDetection(transform.position, m_rSourceB.transform.position, _planeNormal, _posProjected);
+
+        //For BD
+        check[1] = DotProductSegementDetection(m_rSourceB.transform.position, m_vPosSourceD, _planeNormal, _posProjected);
+
+        //For DC
+        check[2] = DotProductSegementDetection(m_vPosSourceD, m_rSourceC.transform.position, _planeNormal, _posProjected);
+
+        //For CA
+        check[3] = DotProductSegementDetection(m_rSourceC.transform.position, transform.position, _planeNormal, _posProjected);
+
+
+
+        if (check[0] && check[1] && check[2] && check[3])
         {
-            return false;
+            return true;
         }
-        else if (Vector3.Dot(TargetToProj, -_axialVector) < 0)
-        {
-            return false;
-        }
-
-
-        return true;
+        return false;
     }
 
-    Vector3 CalcProjection(Vector3 _targetPos, Vector3 _axialVectorU, Vector3 _axialVectorV)
+    bool DotProductSegementDetection(Vector3 _point1, Vector3 _point2, Vector3 _planeNormal, Vector3 _posProjected)
     {
-        Vector3 PosAToTarget = _targetPos - transform.position;
-        float dotResult = Vector3.Dot(_axialVectorU, PosAToTarget);
-        Vector3 PosProjected_AB = (_axialVectorU * dotResult);
+        Vector3 vectorSegement = _point2 - _point1;
+        Vector3 vectorSegementLeft = Vector3.Cross(vectorSegement, _planeNormal);
+        Vector3 SegementToProjected = _posProjected - _point1;
 
+        return (Vector3.Dot(vectorSegementLeft, SegementToProjected) > 0f) ? false : true;
+    }
 
-        dotResult = Vector3.Dot(_axialVectorV, PosAToTarget);
-        Vector3 PosProjected_AC = (_axialVectorV * dotResult);
+    /// <summary>
+    /// Calculate the projection of the target onto the plane
+    /// </summary>
+    /// <param name="_targetPos">Position of the influenced object</param>
+    /// <param name="_planeNormal">Plane's normal vector</param>
+    /// <returns>The projected position as Vector3</returns>
+    Vector3 CalcProjection(Vector3 _targetPos, Vector3 _planeNormal)
+    {
+        Vector3 TargetToPosA = transform.position - _targetPos;
 
-        return PosProjected_AB + PosProjected_AC + transform.position;
+        Vector3 DirectionProjOnNormal = ((Vector3.Dot(TargetToPosA, _planeNormal) / _planeNormal.sqrMagnitude) * _planeNormal.normalized) + _targetPos;
+
+        float k = ((_targetPos.x - transform.position.x) * _planeNormal.x + (_targetPos.y - transform.position.y) * _planeNormal.y + (_targetPos.z - transform.position.z) * _planeNormal.z) 
+                  / ((_targetPos.x - DirectionProjOnNormal.x) * _planeNormal.x + (_targetPos.y - DirectionProjOnNormal.y) * _planeNormal.y + (_targetPos.z - DirectionProjOnNormal.z) * _planeNormal.z);
+
+        return (k * (DirectionProjOnNormal - _targetPos)) + _targetPos;
     }
 
     public override Vector3 CalcForce(Vector3 _targetPos)
     {
         Vector3 axialVectorU = CalcAxialVector(transform.position, m_rSourceB.transform.position);
         Vector3 axialVectorV = CalcAxialVector(transform.position, m_rSourceC.transform.position);
+        Vector3 planeNormal = CalcPlaneNormal(axialVectorU, axialVectorV);
 
-        Vector3 posProjected = CalcProjection(_targetPos, axialVectorU, axialVectorV);
+        Vector3 posProjected = CalcProjection(_targetPos, planeNormal);
+
+        //Debug//////////
+        PosProjected = posProjected;
+        /////////////////
 
         Vector3 force = CalcAxialVector(_targetPos, posProjected);
         return force * GenerateForce(posProjected, _targetPos);
     }
 
-    
+    Vector3 CalcPlaneNormal(Vector3 _axialVectorU, Vector3 _axialVectorV)
+    {
+        return Vector3.Cross(_axialVectorV, _axialVectorU);
+    }
+
+
+
 
     private void OnDrawGizmos()
     {
@@ -74,5 +126,8 @@ public class SingularityPlanarComponent : SingularityComponent
         Gizmos.DrawSphere(m_rSourceB.transform.position, 0.25f);
         Gizmos.DrawSphere(m_rSourceC.transform.position, 0.25f);
         Gizmos.DrawSphere(posD, 0.25f);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(PosProjected, 0.15f);
     }
 }
